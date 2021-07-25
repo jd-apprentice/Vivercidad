b3ebf9abefd902689f1a2584a36a32cbd401a871
guillermoBabangit <guillermo.baban@hotmail.com>
2021-07-25 18:19:41 -0300

    login casi terminado

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,49 @@
 const loginF = document.querySelector("#lgForm");
 const userName = document.querySelector("#userName");
-const loginE = document.querySelector("#lgEmail");
-const loginP = document.querySelector("#lgPass");
 const googleAcc = document.querySelector("#googleAcc");
-var provider = new firebase.auth.GoogleAuthProvider();
+const btnLogin = document.querySelector("#loginBtn");
+const btnCreateU = document.querySelector("#createU");
+const btnLogout = document.querySelector("#btnLogout");
+const formCreateUsr = document.querySelector("#usr-form");
+
+let btns = () => {
+  btnLogin.style.display = "none";
+  googleAcc.style.display = "none";
+  btnCreateU.style.display = "none";
+  btnLogout.style.display = "block";
+  btnLogout.style.margin = "0 auto";
+};
+
+formCreateUsr.addEventListener("click", (e) => {
+  e.preventDefault();
+  let userName = document.querySelector("#email");
+  let password = document.querySelector("#pass");
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(userName.value, password.value)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log("Se creo correctamente");
+      // ...
+    })
+    .catch((error) => {
+      console.log(error);
+      // ..
+    });
+});
 
 loginF.addEventListener("submit", (e) => {
   e.preventDefault();
+  let loginE = document.querySelector("#lgEmail");
+  let loginP = document.querySelector("#lgPass");
   firebase
     .auth()
     .signInWithEmailAndPassword(loginE.value, loginP.value)
     .then((userCredential) => {
       var user = userCredential.user;
-      console.log(user);
+      console.log("Exito");
+      btns();
       loginF.reset();
     })
     .catch((error) => {
@@ -20,41 +51,33 @@ loginF.addEventListener("submit", (e) => {
     });
 });
 
+//Movile loggin
 googleAcc.addEventListener("click", (e) => {
+  const provider = new firebase.auth.GoogleAuthProvider();
   firebase
     .auth()
     .signInWithPopup(provider)
     .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
+      console.log("bien ahi");
+      btns();
     })
     .catch((error) => {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
+      console.log(error.message);
     });
 });
 
-//Observador de estado
-// firebase.auth().onAuthStateChanged((user) => {
-//   if (user) {
-//     // User is signed in, see docs for a list of available properties
-//     // https://firebase.google.com/docs/reference/js/firebase.User
-//     var uid = user.uid;
-//     // ...
-//   } else {
-//     // User is signed out
-//     // ...
-//   }
-// });
+btnLogout.addEventListener("click", () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      console.log("salimo");
+      btnLogin.style.display = "block";
+      googleAcc.style.display = "block";
+      btnCreateU.style.display = "block";
+      btnLogout.style.display = "none";
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
