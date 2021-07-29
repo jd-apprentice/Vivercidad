0a034f3b164ef83b0c7bc919ead2ba3ba2f2f689
guillermoBabangit <guillermo.baban@hotmail.com>
2021-07-29 20:45:38 -0300

    nuevo swiper

--- js/añadirProducto.js
+++ js/añadirProducto.js
@@ -1,7 +1,11 @@
 var storage = firebase.storage();
 const db = firebase.firestore();
 const btnSubir = document.querySelector("#lgForm");
+const grabLista = document.querySelector("#listaProductos");
+const grabCheck = document.querySelector("#productoPadre");
+const grabCheckText = document.querySelector("#productoPadreTexto");
 
+let botonDisplay = false;
 let archivo = document.querySelector("#fileItem");
 let fileAll = "";
 let fileName = "";
@@ -13,12 +17,26 @@ db.collection("saveContador")
     cont = doc.data().contador;
   });
 
+// grabCheck.addEventListener("click", () => {
+//   if (botonDisplay == false) {
+//     grabCheckText.innerHTML = `
+//     <span></span><button class="btn btn-success" type="submit">
+//     Editar
+//   </button>`;
+//     botonDisplay = true;
+//   } else if (botonDisplay == true) {
+//     grabCheckText.innerHTML = `<span></span>`;
+//     botonDisplay = false;
+//   }
+// });
+
 archivo.addEventListener("change", () => {
   fileAll = archivo.files[0];
   fileName = archivo.files[0].name;
 });
 
 btnSubir.addEventListener("submit", async (e) => {
+  e.preventDefault();
   const nameProduct = document.querySelector("#nameProduct").value;
   const precioProduct = document.querySelector("#precioP").value;
   var storageRef = firebase.storage().ref(`imagenes/${nameProduct}`);
@@ -37,4 +55,19 @@ btnSubir.addEventListener("submit", async (e) => {
   await saveContador.set({
     contador: cont,
   });
+
+  let createLabel = document.createElement("label");
+  let createInput = document.createElement("input");
+  let createSpan = document.createElement("span");
+
+  createLabel.classList.add("list-group-item");
+  createInput.classList.add("form-check-input", "me-1");
+  createInput.value = "";
+  createInput.type = "checkbox";
+  createSpan.textContent = `${nameProduct}`;
+
+  createLabel.appendChild(createInput);
+  createLabel.appendChild(createSpan);
+
+  grabLista.appendChild(createLabel);
 });
