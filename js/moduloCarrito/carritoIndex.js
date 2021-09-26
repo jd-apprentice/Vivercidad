4d2fc0796ac8b9a45f32b253f8b91a562ca90e12
Jonathan Dyallo <Emeraldusk@gmail.com>
2021-09-26 00:39:46 -0300

    Carrito compras refactor (#28)
    * carrito eliminar, cantidad, vaciar

--- js/moduloCarrito/carritoIndex.js
+++ js/moduloCarrito/carritoIndex.js
@@ -2,11 +2,14 @@
 import { Render } from "./Render/render.js";
 import { refCarrito } from "./Producto/producto.js";
 import { userName } from "../index.js";
+import { limpiarCarrito, eliminarProducto } from "./Borrar/borrar.js";
+import { actualizarCantidad } from "./Cantidad/cantidad.js";
 import { limpiarCarrito } from "./Borrar/borrar.js";
 
 // Firebase
 export const getCarrito = document.querySelector("#tbodyCarrito");
 
+// Render
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     refCarrito
@@ -18,8 +21,9 @@ firebase.auth().onAuthStateChanged((user) => {
           const renderCarrito = new Render(getCarrito, itemsCarrito.productos);
           renderCarrito.render();
           renderCarrito.addEventListener(vaciarCarrito);
-          renderCarrito.changeValue(updateValue);
+          renderCarrito.changeValue(updateCantidad);
           renderCarrito.deleteProduct(borrarItem);
+          renderCarrito.updateTotal();
         }
       });
   }
@@ -32,14 +36,17 @@ const vaciarCarrito = () => {
   carritoRender.render();
 };
 
-const updateValue = (e) => {
-  const id = e.target.dataset.id;
-  const cantidad = e.target.value;
-}
-
+// Borrar item
 const borrarItem = (e) => {
-  const id = e.target.classList[2];
+  const id = e.target.id;
   const contenido = e.target.closest("tr");
+  eliminarProducto(userName.uid, id);
   contenido.remove();
 }
 
+// Actualizar cantidad
+const updateCantidad = (e) => {
+  const id = e.target.id;
+  const cantidad = e.target.value;
+  actualizarCantidad(userName.uid, id, cantidad);
+}
